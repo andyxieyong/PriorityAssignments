70e929809f27bc9483d1d608b4b4e8ee632d5e9f
zs673 <zhaoshuai0521@hotmail.com>
2019-05-30 04:34:46 +0100

    ds

--- priorityTest.sh
+++ priorityTest.sh
@@ -8,4 +8,6 @@ cd /home/userfs/z/zs673/PriorityAssignment
 pwd
 rm nohup.out
 
+javac $(find ./src/* | grep .java)
+
 LD_LIBRARY_PATH=src nohup java -cp /home/userfs/z/zs673/PriorityAssignment/bin evaluation.CompleteExperiments &
