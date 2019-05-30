8a3c4f4e7c93c4042bb59bad567df6a481b81da5
zs673 <zhaoshuai0521@hotmail.com>
2019-05-30 04:40:52 +0100

    s

--- priorityTest.sh
+++ priorityTest.sh
@@ -1,13 +1,17 @@
 
 
-wcd /home/userfs/z/zs673/PriorityAssignments/result
+cd /home/userfs/z/zs673/PriorityAssignments/result
 pwd
 
 rm -rf *.txt
+
 cd /home/userfs/z/zs673/PriorityAssignments
 pwd
 rm nohup.out
 
 javac $(find ./src/* | grep .java)
 
-LD_LIBRARY_PATH=src nohup java -cp /home/userfs/z/zs673/PriorityAssignments/bin evaluation.CompleteExperiments &
+
+nohup java -cp src/ evaluation.CompleteExperiments &
+
+#LD_LIBRARY_PATH=src nohup java -cp /home/userfs/z/zs673/PriorityAssignments/bin evaluation.CompleteExperiments &
